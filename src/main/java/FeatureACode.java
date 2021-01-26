9ef5d68d5504f851f8899a9de3ef42b0f45455cb
Marwan Subhi <marwan.subhi4@gmail.com>
2021-01-26 14:23:11 -0500

    more work on feature A

--- src/main/java/FeatureACode.java
+++ src/main/java/FeatureACode.java
@@ -2,5 +2,6 @@ public class FeatureACode {
 
     public static void main(String[] args) {
         System.out.println("Awesome work for A start");
+        System.out.println("B21 learning more stuff");
     }
 }
